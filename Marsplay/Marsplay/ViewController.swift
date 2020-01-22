d17319136e2e8eb96f5eca350448dd7e366911d1
Amol Prakash <amolprakash06@gmail.com>
2020-01-22 11:18:42 +0530

    Project Initial Commit

---
+++ Marsplay/Marsplay/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Marsplay
+//
+//  Created by Amol Prakash on 22/01/20.
+//  Copyright © 2020 Amol Prakash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
