e4b0df51d0ffcf0ec81be0f1dbee48ee1c70a7b3
Amol Prakash <amolprakash06@gmail.com>
2020-02-03 13:48:13 +0530

    Restructuring folders and removed retail cycle in detail screen.

--- Marsplay/Marsplay/MovieDetails/Presenter/MovieDetailsPresenter.swift
+++ Marsplay/Marsplay/MovieDetails/Presenter/MovieDetailsPresenter.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class MovieDetailsPresenter: MovieDetailsPresenterProtocol {
     
-    var view: MovieDetailsViewProtocol?
+    weak var view: MovieDetailsViewProtocol?
     var movie: Movie?
     
     required init(movie: Movie) {
